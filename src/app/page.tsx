87b1ac27e54cb6abf2b5231c4cda90ac2b172fef
Global Name <zapekinnikitamain@gmail.com>
2026-01-13 12:47:41 +0300

    feat(welcome): Add welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import styles from "./page.module.css";
+import WelcomeBlock from "./_components/WelcomeBlock";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-    </div>
-  );
+  return <WelcomeBlock />;
 }
